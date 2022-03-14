a67fb124c67daca8e9a301955ca7818e89735ed0
Andrew Dutson <Dutsandrew@gmail.com>
2022-03-14 08:14:39 -0600

    styling page and moving HTML items for better placement

--- app.js
+++ app.js
@@ -3,25 +3,41 @@ const bookFormContainer = document.querySelector('.form-container');
 const submitBookForm = document.querySelector('.submit-book-form');
 const bookLibrary = document.querySelector('.book-library');
 const bookCards = document.querySelectorAll('.book-cards');
+const form = document.querySelector('.form-container');
 
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const readItYes = document.querySelector('#read-it-yes');
-const readItNo = document.querySelector('#read-it-no');
+const title = document.querySelector('#title').value;
+const author = document.querySelector('#author').value;
+const pages = document.querySelector('#pages').value;
+const haveIReadIt = document.querySelector('.have-i-read-it').value;
+const slider = document.querySelector('.slider').value;
+
+function openForm() {
+    document.getElementById("form").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("form").style.display = "none";
+  }
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
     this.getInfo = function() {
-        return(`${title}, by ${author}, ${pages}, ${read}`);
-    }
+        return(`${title}, by ${author}, ${pages}`);
+    };
 }
 
 function addBookToLibrary() {
+    const book = new Book();
 
+    myLibrary.push(book);
 }
+
+
+
+addBookToLibrary('The Lightning Thief', 'Rick Riordan', 333);
+addBookToLibrary('The Lightning Thief', 'Rick Riordan', 333);
+
+form.addEventListener('submit', addBookToLibrary);
