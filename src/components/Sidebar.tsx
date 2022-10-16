cb5065ce3723e1ea840f5f6e09e9139ba9090b0d
Andrew Dutson <Dutsandrew@gmail.com>
2022-10-16 15:42:29 -0600

    form validation converted to ts and react

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,6 @@
 import React, { FC, useState, MouseEvent } from 'react';
 import '../style/Sidebar.css';
 
-interface AppProps {
-  openBookForm: React.MouseEventHandler<HTMLButtonElement> | undefined,
-}
-
 const Sidebar = () => {
 
   const [formToggle, setFormToggle] = useState({
@@ -48,6 +44,63 @@ const Sidebar = () => {
     }
   }
 
+  const handleBookFormChange = (e: any | null): void => {
+    const entryThatChanged: any | null = e.target;
+    const errorText: any | null = e.target.nextSibling;
+    if (entryThatChanged && errorText) {
+      if (entryThatChanged.validity.valid) {
+        errorText.textContent = "";
+        errorText.className = "error";
+      } else {
+        showError(entryThatChanged, errorText);
+      };
+    };
+  };
+
+  const showError = (entry: any | null, error: any | null): void => {
+    if (entry.id === 'title') {
+      if (entry.validity.valueMissing) {
+        error.textContent = "You need to enter a book title";
+        error.classList.add("error", "error-active");
+      } else if (entry.validity.tooShort) {
+        error.textContent = `Book should be at least ${entry.minLength} characters; you entered: ${entry.value.length}`;
+        error.classList.add("error", "error-active");
+      } else if (entry.validity.tooLong) {
+        error.textContent = `Book should be no more than ${entry.maxLength} characters; you entered: ${entry.value.length}`;
+        error.classList.add("error", "error-active");
+      };
+      return;
+    };
+
+    if (entry.id === 'author') {
+      if (entry.validity.valueMissing) {
+        error.textContent = "You need to enter the author's name";
+        error.classList.add("error", "error-active");
+      } else if (entry.validity.tooShort) {
+        error.textContent = `The author name should be at least ${entry.minLength} characters; you entered: ${entry.value.length}`;
+        error.classList.add("error", "error-active");
+      } else if (entry.validity.tooLong) {
+        error.textContent = `The author name should be no more than ${entry.maxLength} characters; you entered: ${entry.value.length}`;
+        error.classList.add("error", "error-active");
+      };
+      return;
+    };
+   
+    if (entry.id === 'pages') {
+      if (entry.validity.valueMissing) {
+        error.textContent = "You need to include the number of pages";
+        error.classList.add("error", "error-active");
+      } else if (entry.validity.tooShort) {
+        error.textContent = `The amount of pages must be at least ${entry.minLength} characters; you entered: ${entry.value.length}`;
+        error.classList.add("error", "error-active");
+      } else if (entry.validity.tooLong) {
+        error.textContent = `The amount of pages must be less than ${entry.maxLength} characters; you entered: ${entry.value.length}`;
+        error.classList.add("error", "error-active");
+      };
+    };
+    return;
+  };
+
 
   return (
     <div className="sidebar">
@@ -56,15 +109,15 @@ const Sidebar = () => {
             <fieldset>
               <legend className="legend-text">Add new book to library</legend>
               <label htmlFor="title" className="label-title">Title:</label>
-              <input type="text" className="input" id="title" placeholder="Title" maxLength={25} minLength={4} required></input>
+              <input type="text" className="input" id="title" placeholder="Title" maxLength={25} minLength={4} onChange={handleBookFormChange} required></input>
               <p className ="error-msg" id="title-error"></p>
 
               <label htmlFor="author" className="label-title">Author:</label>
-              <input type="text" className="input" id="author" placeholder="Author" maxLength={30} minLength={4} required></input>
+              <input type="text" className="input" id="author" placeholder="Author" maxLength={30} minLength={4} onChange={handleBookFormChange} required></input>
               <p className ="error-msg" id="author-error"></p>
 
               <label htmlFor="pages" className="label-title">Pages:</label>
-              <input type="number" className="input" id="pages" placeholder="# of pages" maxLength={5} minLength={2} required></input>
+              <input type="number" className="input" id="pages" placeholder="# of pages" maxLength={5} minLength={2} onChange={handleBookFormChange} required></input>
               <p className ="error-msg" id="pages-error"></p>
 
               <input type="button" name="submit" className="submit-book-form" value="Add Book"></input>
