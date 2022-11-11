317974661cdb82a77a071771e003f41d0c93a944
Andrew Dutson <Dutsandrew@gmail.com>
2022-11-11 16:03:13 -0700

    firebase => github compatibility additions

---
+++ src/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyB25C0MNgL_tEDdOGN759b1NX0ZgHC5bj8",
+  authDomain: "library-app-7b48d.firebaseapp.com",
+  projectId: "library-app-7b48d",
+  storageBucket: "library-app-7b48d.appspot.com",
+  messagingSenderId: "243144925550",
+  appId: "1:243144925550:web:541a504b3ae60cb2c8a17f",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
